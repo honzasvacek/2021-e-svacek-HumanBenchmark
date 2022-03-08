02128da5569dfa2635599f6b91eea5c13d12bc41
honzasvacek <94893997+honzasvacek@users.noreply.github.com>
2022-03-08 17:39:22 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+package application;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Circle;
+
+public class Main extends Application {
+	
+
+	public void start(Stage stage) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+			Parent root = loader.load();
+			Controller controller = loader.getController();
+			Scene scene = new Scene(root);
+			
+			scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+				@Override
+				public void handle(KeyEvent event) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+			
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setResizable(false);
+			stage.setScene(scene);
+			stage.setTitle("Human Abilities Game");
+			stage.setFullScreen(false);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
